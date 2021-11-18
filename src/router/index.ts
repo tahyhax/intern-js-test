37eee4ef13a46f675bc4356d9f510424f0189182
dmytro.bekeshenko <dmytro.bekeshenko@onix-systems.com>
2021-11-18 13:56:42 +0200

    lesson-6 add  error  page 404

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,13 +3,17 @@ import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
+    name: 'Tasks',
+    component: () => import('../views/Tasks.vue')
+  },
+  {
+    path: '/activity',
     name: 'Activity',
     component: () => import('../views/Activity.vue')
   },
   {
-    path: '/tasks',
-    name: 'Tasks',
-    component: () => import('../views/Tasks.vue')
+    path: '/:pathMatch(.*)*',
+    component: () => import('../views/errors/404.vue')
   }
 ]
 
