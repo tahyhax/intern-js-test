d9e75edf54bb6d4100abdb5d6e30b0a6d3d2136a
dmytro.bekeshenko <dmytro.bekeshenko@onix-systems.com>
2021-12-10 14:23:35 +0200

    lesson-8 add  animation

--- src/views/Tasks.vue
+++ src/views/Tasks.vue
@@ -6,6 +6,7 @@
     task-form(@onSubmit="addTask")
     transition-group(class="task__list" tag="div" name="bounce")
       task-item(v-for="(task, key) in tasks"
+        :ref="setItemRef"
         :task="task"
         :index="key"
         :key="`task-${key}`"
@@ -14,7 +15,7 @@
       )
 </template>
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, onMounted, reactive } from 'vue'
 import { ITasks } from '@/types/tasks'
 import TaskForm from '@/components/Task/TaskFrom.vue'
 import TaskItem from '@/components/Task/TaskItem.vue'
@@ -25,13 +26,8 @@ export default defineComponent({
     TaskForm,
     TaskItem
   },
-  data () {
-    return {
-      tasks: [] as Array<ITasks>
-    }
-  },
-  created () {
-    this.tasks = [
+  setup: function () {
+    const tasks = reactive<Array<ITasks>>([
       {
         title: 'task 1',
         text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged',
@@ -50,18 +46,43 @@ export default defineComponent({
         status: 'open',
         date: '2021-12-09 14:26'
       }
-    ]
-  },
-  methods: {
-    addTask (task: ITasks): void {
-      this.tasks.push(task)
-    },
-    destroyTask (index: number): void {
-      this.tasks.splice(index, 1)
-    },
-    completeTask (index: number) {
-      this.tasks[index].status = 'complete'
+    ])
+    const itemRefs: Array<HTMLElement> = []
+    const setItemRef = (el: any) => {
+      if (el) {
+        itemRefs.push(el.$el)
+      }
+      return itemRefs
     }
+    const addTask = (task: ITasks): void => {
+      tasks.push(task)
+    }
+    const destroyTask = (index: number): void => {
+      tasks.splice(index, 1)
+    }
+
+    const completeTask = (index: number): void => {
+      tasks[index].status = 'complete'
+    }
+    // NOTE хотел сделать так но получил ошибку. Можешь сказать что я не так сделал ?
+    // const fontScale = (list: [], delay: number, animationClass: string) => {
+    //   list.every((el: HTMLElement, index) => {
+    //     setTimeout(() => {
+    //       el.classList.add(animationClass)
+    //     }, index * delay)
+    //   })
+    // }
+
+    onMounted(() => {
+      itemRefs.forEach((el: HTMLElement, index) => {
+        setTimeout(() => {
+          el.classList.add('grow-animation')
+        }, index * 150)
+      })
+      // NOTE TS2345: Argument of type 'HTMLElement[]' is not assignable to parameter of type '[]'. Target allows only 0 element(s) but source may have more.
+      // fontScale(itemRefs, 100, 'grow-animation')
+    })
+    return { setItemRef, tasks, addTask, destroyTask, completeTask }
   }
 })
 </script>
@@ -91,29 +112,46 @@ export default defineComponent({
     flex-direction: column;
   }
 
-  //animation
-  .bounce-enter-active {
-    animation: bounce-in .4s ease-out both;
-  }
+}
 
-  .bounce-leave-active {
-    animation: bounce-in .4s reverse ease-in both;
-  }
+//animation
+.bounce-enter-active {
+  animation: bounce-in .4s ease-out both;
+}
 
-  //keyframe
-  @keyframes bounce-in {
-    0% {
-      transform: scale(.4);
-    }
-    50% {
-      transform: scale(0.6);
-    }
-    75% {
-      transform: scale(0.9);
-    }
-    100% {
-      transform: scale(1);
-    }
+.bounce-leave-active {
+  animation: bounce-in .4s reverse ease-in both;
+}
+
+.grow-animation {
+  animation-name: grow;
+  animation-duration: 2s;
+}
+
+//keyframes
+@keyframes grow {
+  0% {
+  }
+  75% {
+    transform: scale(1.1, 1.1);
+  }
+  100% {
+    transform: scale(1, 1);
+  }
+}
+
+@keyframes bounce-in {
+  0% {
+    transform: scale(0.3);
+  }
+  50% {
+    transform: scale(0.6);
+  }
+  75% {
+    transform: scale(0.9);
+  }
+  100% {
+    transform: scale(1);
   }
 }
 </style>
