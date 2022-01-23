c62b628ce128bdb265c618f09cac71c6fe67e53b
dmytro.bekeshenko <dmytro.bekeshenko@onix-systems.com>
2022-01-23 17:48:31 +0200

    lesson-12 use store  date

--- src/views/Activity.vue
+++ src/views/Activity.vue
@@ -23,55 +23,19 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { computed, defineComponent } from 'vue'
+import { useStore } from 'vuex'
 export default defineComponent({
   name: 'activity',
-  data () {
-    return {
-      activities: [
-        {
-          text: 'Darika Samak mark as done Listing on Product Hunt so that we can reach as many potential users',
-          type: 'complete',
-          created: '8:40 PM'
-        },
-        {
-          text: 'Emilee Simchenko commented on Account for teams and personal in bottom style',
-          type: 'message',
-          created: '7:32 PM'
-        },
-        {
-          text: 'During a project build, it is necessary to evaluate the product design and development against project requirements and outcomes',
-          type: 'answer',
-          created: ''
-        },
-        {
-          text: 'Darika Samak uploaded 4 files on An option to search in current projects or in all projects',
-          type: 'attachment',
-          created: '6:02 PM',
-          attachments: [
-            {
-              src: 'https://via.placeholder.com/100?text=Image 1',
-              description: 'Image 1'
-            },
-            {
-              src: 'https://via.placeholder.com/100?text=Image 2',
-              description: 'Image 2'
-            },
-            {
-              src: 'https://via.placeholder.com/100?text=Image 3',
-              description: 'Image 3'
-            },
-            {
-              src: 'https://via.placeholder.com/100?text=Image 4',
-              description: 'Image 4'
-            }
-          ]
-        }]
+  setup: function (props, { emit }) {
+    const store = useStore()
+    const activities = computed(() => store.state.activity.activities)
+    const updateNotification = (index: number) :void => {
+      emit('update-notification', index)
     }
-  },
-  methods: {
-    updateNotification (index:number):void {
-      this.$emit('update-notification', index)
+    return {
+      activities,
+      updateNotification
     }
   }
 })
