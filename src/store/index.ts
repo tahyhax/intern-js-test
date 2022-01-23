677b5f6a44696339b04a7ffb31aea54c495092de
dmytro.bekeshenko <dmytro.bekeshenko@onix-systems.com>
2022-01-23 18:12:21 +0200

    lesson-12 install  packege  vuex-persistence and  use  it

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,12 @@
-import { createStore } from 'vuex'
+import { createStore, Store } from 'vuex'
 import task from '@/store/modules/task'
 import kanban from '@/store/modules/kanban'
 import activity from '@/store/modules/activity'
+import VuexPersistence from 'vuex-persist'
+
+const VuexLocal = new VuexPersistence({
+  storage: window.localStorage
+})
 
 export default createStore({
   state: {},
@@ -11,5 +16,6 @@ export default createStore({
     task,
     kanban,
     activity
-  }
+  },
+  plugins: [VuexLocal.plugin]
 })
