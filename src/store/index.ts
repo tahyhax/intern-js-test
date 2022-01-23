865c4872ffbe4cfd2c5596b9d2f01e01e61a3070
dmytro.bekeshenko <dmytro.bekeshenko@onix-systems.com>
2022-01-23 17:44:15 +0200

    lesson-12 use new modules

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,15 @@
 import { createStore } from 'vuex'
+import task from '@/store/modules/task'
+import kanban from '@/store/modules/kanban'
+import activity from '@/store/modules/activity'
 
 export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+  state: {},
+  mutations: {},
+  actions: {},
   modules: {
+    task,
+    kanban,
+    activity
   }
 })
